8d3919ea60b923224addbe6d87a5251fe2b4f7eb
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-12-01 14:46:33 -0300

    criando entity de anuncio

---
+++ src/main/java/com/br/uff/anuncios/model/Anuncio.java
@@ -0,0 +1,14 @@
+package com.br.uff.anuncios.model;
+
+import java.util.List;
+
+public class Anuncio {
+
+    private Long id;
+    private String descricao;
+    private Double preco;
+    private String localizacao;
+    private Usuario usuario;
+
+
+}
