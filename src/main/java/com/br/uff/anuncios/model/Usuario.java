c81039eea651eb019de4c40119a71ac8ed0d2072
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-12-01 14:42:04 -0300

    criando entity de usuario

--- src/main/java/com/br/uff/anuncios/model/Usuario.java
+++ src/main/java/com/br/uff/anuncios/model/Usuario.java
@@ -1,4 +1,18 @@
 package com.br.uff.anuncios.model;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+    private Integer reputacao;
+    private List<String> historicoDeTrancacoes;
+
 }
