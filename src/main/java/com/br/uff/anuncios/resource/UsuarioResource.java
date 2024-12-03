ac20255dc45efc7f1f518226bf7cbb83fef72d12
João Vicente Gaidzinski <gaidzinski2110@gmail.com>
2024-12-03 11:40:24 -0300

    Adiciona conexão com BD, mapeia, cadastra e busca usuario

--- src/main/java/com/br/uff/anuncios/resource/UsuarioResource.java
+++ src/main/java/com/br/uff/anuncios/resource/UsuarioResource.java
@@ -1,8 +1,10 @@
 package com.br.uff.anuncios.resource;
 
+import com.br.uff.anuncios.dto.UsuarioRecordDTO;
 import com.br.uff.anuncios.model.Usuario;
 import com.br.uff.anuncios.service.UsuarioService;
 import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +19,14 @@ public class UsuarioResource {
     private UsuarioService service;
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<Usuario> createChat(@RequestBody @Valid Usuario usuario){
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(usuario));
+    public ResponseEntity<Usuario> createChat(@RequestBody @Valid UsuarioRecordDTO usuario){
+        Usuario u = new Usuario();
+        BeanUtils.copyProperties(usuario, u);
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(u));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getById(@PathVariable String id){
+    public ResponseEntity<Usuario> getById(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
     }
 
--- src/main/java/com/br/uff/anuncios/service/UsuarioService.java
+++ src/main/java/com/br/uff/anuncios/service/UsuarioService.java
@@ -1,17 +1,32 @@
 package com.br.uff.anuncios.service;
 
 import com.br.uff.anuncios.model.Usuario;
+import com.br.uff.anuncios.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
 
-    public Usuario findById(String id){
-        return null;
+    private final UsuarioRepository repository;
+    @Autowired
+    public UsuarioService(UsuarioRepository repository){ this.repository = repository;}
+
+    @Transactional
+    public Usuario findById(Long id){
+        Optional<Usuario> optUser = repository.findById(id);
+        return optUser.orElse(null);
     }
 
+    @Transactional
     public Usuario save(Usuario usuario){
-        return null;
+        repository.save(usuario);
+        return repository.save(usuario);
+
     }
 
 }
