d40254e5006810fd2866e5aabc9825e72534e89e
Rodrigo Nogueira <nogueiragm22@hotmail.com>
2024-12-07 16:40:41 -0300

    Criando enums

--- src/main/java/com/br/uff/anuncios/model/Anuncio.java
+++ src/main/java/com/br/uff/anuncios/model/Anuncio.java
@@ -1,14 +1,45 @@
 package com.br.uff.anuncios.model;
 
+import com.br.uff.anuncios.model.enums.Categoria;
+import com.br.uff.anuncios.model.enums.TipoAnuncio;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
 import java.util.List;
 
+
+@Entity
+@Table(name="anuncio")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Anuncio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String descricao;
-    private Double preco;
-    private String localizacao;
-    private Usuario usuario;
+    @Lob
+    private byte[] foto;
+    @Column(precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_anuncio", nullable = false)
+    private TipoAnuncio tipoAnuncio;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Categoria categoria;
+
+    @Column(nullable = false, length = 255)
+    private String endereco;
+
+    @Column(name = "id_usuario", nullable = false)
+    private Long idUsuario;
 
 
 }
---
+++ src/main/java/com/br/uff/anuncios/model/enums/Categoria.java
@@ -0,0 +1,11 @@
+package com.br.uff.anuncios.model.enums;
+
+public enum Categoria {
+
+    Livro,
+    Roupa,
+    Móvel,
+    Aula_Particular, // Substituí espaço por underline
+    Tecnologia,
+    Imóvel;
+}
