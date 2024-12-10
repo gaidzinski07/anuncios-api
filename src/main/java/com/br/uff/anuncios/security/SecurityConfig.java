7f068f4eb17182e5c42bf3e8bc5f136dc9d8857a
João Victor Souza <jvsouzasantos@id.uff.br>
2024-12-09 23:34:17 -0300

    feat(security): adicionar configuração de segurança para rotas públicas e privadas

---
+++ src/main/java/com/br/uff/anuncios/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.br.uff.anuncios.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+  
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> {
+      //Adicione rotas publicas aqui
+      auth.requestMatchers("/usuario/cadastrar").permitAll();
+      auth.anyRequest().authenticated();
+    });
+    return http.build();
+  }
+}
