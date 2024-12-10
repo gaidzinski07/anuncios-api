15df0b8a0853adc5a96aeff460b9411a25e082ff
João Victor Souza <jvsouzasantos@id.uff.br>
2024-12-09 23:44:47 -0300

    feat(security): adicionar PasswordEncoder para criptografia de senhas

--- src/main/java/com/br/uff/anuncios/service/UsuarioService.java
+++ src/main/java/com/br/uff/anuncios/service/UsuarioService.java
@@ -5,6 +5,7 @@ import com.br.uff.anuncios.repository.UsuarioRepository;
 import com.br.uff.anuncios.util.EmailUtils;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
@@ -16,6 +17,9 @@ public class UsuarioService {
     private final UsuarioRepository repository;
     @Autowired
     public UsuarioService(UsuarioRepository repository){ this.repository = repository;}
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Transactional
     public Usuario findById(Long id){
@@ -29,6 +33,10 @@ public class UsuarioService {
         if(!EmailUtils.isEmailUff(usuario.getEmail())){
             throw new IllegalArgumentException("O email informado deve estar num domínio da Universidade Federal Fluminense.");
         }
+
+        var password = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(password);
+
         repository.save(usuario);
         return repository.save(usuario);
 
