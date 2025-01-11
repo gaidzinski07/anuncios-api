f1809b501f399536c5afd5d547a9483b0f85b939
João Victor Souza <jvsouzasantos@id.uff.br>
2025-01-11 19:34:00 -0300

    refactor: add docker compose for database

--- src/main/java/com/br/uff/anuncios/model/Anuncio.java
+++ src/main/java/com/br/uff/anuncios/model/Anuncio.java
@@ -1,38 +1,38 @@
 package com.br.uff.anuncios.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
-import java.util.List;
 
 @Entity
-@Table(name="anuncio")
+@Table(name = "anuncio")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class Anuncio  implements Serializable {
+public class Anuncio implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private String descricao;
+
     @Column
     private Double preco;
+
     @Column
     private String endereco;
+
     @Column
     private String categoria;
+
     @Column
     private String tipoAnuncio;
-    @ManyToOne(fetch=FetchType.EAGER)
+
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
-
-
 }
--- src/main/java/com/br/uff/anuncios/model/Usuario.java
+++ src/main/java/com/br/uff/anuncios/model/Usuario.java
@@ -7,25 +7,32 @@ import java.io.Serializable;
 import java.util.List;
 
 @Entity
-@Table(name="usuario")
+@Table(name = "usuario")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class Usuario implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private String nome;
+
     @Column
     private String email;
+
     @Column
     private String senha;
+
     @Column
     private String telefone;
+
     @Transient
     private Integer reputacao;
-    //private List<String> historicoDeTrancacoes;
 
+    @OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY)
+    private List<Anuncio> anuncios;
 }
